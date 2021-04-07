d5366265c05a18342e4ed40c9ba698258b4b0da0
Luan Andrade <luansandrade92@hotmail.com>
2021-04-06 21:58:35 -0300

    Create statefulWidget for home and update texts

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home.dart';
+
 void main() {
-  runApp(MaterialApp(
-      title: "Contador de Pessoas",
-      home: Stack(
-        children: [
-          Image.asset(
-            "images/restaurant.jpg",
-            fit: BoxFit.cover,
-            height: 1000.0,
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Pessoas: 0",
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: TextButton(
-                        onPressed: null,
-                        child: Text(
-                          "-1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        )),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: TextButton(
-                        onPressed: null,
-                        child: Text(
-                          "+1",
-                          style: TextStyle(fontSize: 40.0, color: Colors.white),
-                        )),
-                  )
-                ],
-              ),
-              Text(
-                "Pode entrar!",
-                style: TextStyle(
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontSize: 30.0),
-              )
-            ],
-          )
-        ],
-      )));
+  runApp(MaterialApp(title: "Contador de Pessoas", home: Home()));
 }
